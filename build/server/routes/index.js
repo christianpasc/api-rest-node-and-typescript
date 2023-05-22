dd189a5f069da27cbe82dfc0225232ceb2e656dd
christian_pasc <christianpasc@gmail.com>
2023-05-22 16:16:57 -0300

    modified:   .gitignore 	new file:   build/server/controllers/cities/Create.js 	new file:   build/server/controllers/cities/DeleteById.js 	new file:   build/server/controllers/cities/GetAll.js 	new file:   build/server/controllers/cities/GetById.js 	new file:   build/server/controllers/cities/UpdateById.js 	new file:   build/server/controllers/cities/index.js 	new file:   build/server/controllers/index.js 	new file:   build/server/database/index.js 	modified:   build/server/routes/index.js 	modified:   build/server/server.js 	new file:   build/server/shared/middleware/Validation.js 	new file:   build/server/shared/middleware/index.js 	new file:   build/server/shared/services/TranslationsYup.js 	new file:   build/server/shared/services/index.js 	new file:   jest.config.ts 	modified:   package.json 	new file:   tests/jest.setup.ts 	modified:   tsconfig.json 	modified:   yarn.lock

--- build/server/routes/index.js
+++ build/server/routes/index.js
@@ -2,10 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
-const http_status_codes_1 = require("http-status-codes");
+const controllers_1 = require("./../controllers");
 const router = (0, express_1.Router)();
 exports.router = router;
-router.post('/teste', (req, res) => {
-    console.log(req.body);
-    return res.status(http_status_codes_1.StatusCodes.UNAUTHORIZED).json(req.body);
+router.get('/', (req, res) => {
+    return res.send('Ola Dev!');
 });
+router.get('/cities', controllers_1.CitiesController.getAllValidation, controllers_1.CitiesController.getAll);
+router.get('/cities/:id', controllers_1.CitiesController.getByIdValidation, controllers_1.CitiesController.getByID);
+router.put('/cities/:id', controllers_1.CitiesController.updateByIdValidation, controllers_1.CitiesController.updateByID);
+router.delete('/cities/:id', controllers_1.CitiesController.deleteByIdValidation, controllers_1.CitiesController.deleteByID);
+router.post('/cities', controllers_1.CitiesController.createValidation, controllers_1.CitiesController.create);
